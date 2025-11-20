c47ddd714e3dc17d743ed8ba504322872e20100b
Aman Bharti <amanbharti0112@gmail.com>
2025-11-20 13:02:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,19 @@
 variable "ami" {
   description = "EC2 AMI ID"
+  type        = string
 }
 
 variable "vpc_id" {
   description = "VPC ID"
+  type        = string
 }
 
 variable "subnet" {
   description = "Subnet ID"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Existing security group ID"
+  type        = string
 }
