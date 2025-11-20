590baa2c8f77dd7114539390eac805ce896dc048
Aman Bharti <amanbharti0112@gmail.com>
2025-11-20 12:42:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# EC2 Instance (using existing security group)
 resource "aws_instance" "my_ec2" {
   ami                    = var.ami
   instance_type          = "t3.micro"
@@ -10,7 +9,15 @@ resource "aws_instance" "my_ec2" {
   key_name               = "ubuntu"
   associate_public_ip_address = true
 
-  vpc_security_group_ids = ["sg-XXXXXXXXXXXXXXX"]  # <-- put your real SG ID here
+  vpc_security_group_ids = [var.security_group_id]
+
+  user_data = <<-EOF
+#!/bin/bash
+sudo dnf install -y nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+echo "<h1>Deployed using Terraform 🚀</h1>" > /usr/share/nginx/html/index.html
+EOF
 
   tags = {
     Name = "Terraform-EC2"
